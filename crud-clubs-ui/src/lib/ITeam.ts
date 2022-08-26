3490324ddbecb42b4b0c77bcdfdbfbaaaa9ae678
anarod13 <anam.rodriguez13@gmail.com>
2022-08-26 20:56:01 -0300

    Add team interfaces

---
+++ crud-clubs-ui/src/lib/ITeam.ts
@@ -0,0 +1,18 @@
+import type { ITeamArea } from './ITeamArea';
+
+export default interface ITeam {
+	id: number;
+	area: ITeamArea;
+	name: string;
+	shortName: string;
+	tla: string;
+	crestUrl: string;
+	address: string;
+	phone: string | null;
+	website: string;
+	email: string | null;
+	founded: number | null;
+	clubColors: string;
+	venue: string;
+	lastUpdated: string;
+}
