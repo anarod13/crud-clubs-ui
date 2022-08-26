bd7ce8c92cf54b9e19521a9c03bed54ad358f836
anarod13 <anam.rodriguez13@gmail.com>
2022-08-26 20:56:26 -0300

    Add teams components

---
+++ crud-clubs-ui/src/lib/DetailedTeam.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type ITeam from './ITeam';
+
+	export let team: ITeam;
+</script>
+
+<main>
+	<h2>{team.name}</h2>
+	<img src={team.crestUrl} />
+	<div><label>Adress: </label><input type="text" placeholder={team.address} /></div>
+	<div><label>TLA: </label><input type="text" placeholder={team.tla} /></div>
+	<div><label>Phone: </label><input type="text" placeholder={team.phone} /></div>
+	<div><label>Website: </label><input type="text" placeholder={team.website} /></div>
+	<div><label>Email: </label><input type="text" placeholder={team.email} /></div>
+	<div><label>Founded: </label><input placeholder={team.founded} /></div>
+	<div><label>Club Colors: </label><input type="text" placeholder={team.clubColors} /></div>
+	<div><label>Venue: </label><input type="text" placeholder={team.venue} /></div>
+	<div><label>Last Updated: </label><input type="text" placeholder={team.lastUpdated} /></div>
+</main>
+
+<style>
+	main {
+		background-color: blue;
+	}
+</style>
---
+++ crud-clubs-ui/src/lib/Teams.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type ITeam from './ITeam';
+	import teamData from './teams.json';
+	const listedTeams: ITeam[] = teamData;
+
+	let selectedTeam = null;
+
+	function openTeamModal(team: ITeam) {
+		selectedTeam = team;
+	}
+</script>
+
+<main>
+	<p>Currently you have {listedTeams.length} teams listed!</p>
+	<button>Add</button>
+	<table>
+		<tr>
+			<th>Escudo</th>
+			<th>Team</th>
+			<th>Country</th>
+			<th>Actions</th>
+		</tr>
+		{#each listedTeams as team}
+			<tr>
+				<td><img class="crud-clubs-team-logo" src={team.crestUrl} alt={team.tla} /></td>
+				<td>{team.name}</td>
+				<td>{team.area.name}</td>
+				<td><button>Edit</button></td>
+				<td
+					><button
+						on:click={() => {
+							openTeamModal(team);
+						}}>Delete</button
+					></td
+				>
+			</tr>
+		{/each}
+	</table>
+</main>
+
+<style>
+	.crud-clubs-team-logo {
+		width: 90%;
+		height: 90%;
+		box-sizing: border-box;
+	}
+</style>
